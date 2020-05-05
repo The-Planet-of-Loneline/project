0bf19be514f61d11a72d55a4f1d7c93dac4e9fa0
HCLacids <1246311560@qq.com>
2020-05-05 18:16:04 +0800

    login

--- src/pages/component/Dialog.jsx
+++ src/pages/component/Dialog.jsx
@@ -3,9 +3,12 @@ import { View, Text, Image } from '@tarojs/components'
 import './Dialog.scss'
 
 export default class Dialog extends Component{
+    handleClose(){
+        this.props.onHandleClose()
+    }
     render(){
         return(
-        <View className='dialog'>{this.props.children}</View>
+        <View className='dialog' onClick={this.handleClose.bind(this)}>{this.props.children}</View>
         )
     }
 }
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,11 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Image, Input, Button } from '@tarojs/components'
+import { View, Image, Input, Button,Text } from '@tarojs/components'
 import bg from '../img/bg.png'
 import './login.scss'
 import view from '../img/view.png'
 import viewOff from '../img/viewOff.png'
 import Fetch from '../../service/fetch'
+import Toast from './toast/toast'
 
 export default class login extends Component{
     state={
@@ -12,7 +13,9 @@ export default class login extends Component{
         id: '',
         password: '',
         loading: false,
-        powered:false
+        powered:false,
+        mask:false,
+        li: true,
     }
 
     componentWillMount () {
@@ -28,6 +31,9 @@ export default class login extends Component{
         }
         if (password) {
             this.setState({ password })
+        }
+        if(id&&password){
+            this.setState({mask:true})
         }
         Taro.showShareMenu({
             showShareItems: ['qq', 'qzone', 'wechatFriends', 'wechatMoment']
@@ -67,10 +73,28 @@ export default class login extends Component{
             }) 
         }
     }
+    onHandleMask(){
+        const {mask} = this.state
+        if(mask)
+        this.setState({
+           mask:false 
+        })
+        else this.setState({mask:true})
+    }
     
+    onHandleLi(){
+        const{li} = this.state
+        if(li) this.setState({li: false});
+        else this.setState({li:true});
+    }
+
+    handleClose(){
+        this.setState({li:true})
+    }
+
     onHandleLogin(){
-        const {id,password,loading} = this.state
-        if(id && password && !loading){
+        const {id,password,loading,mask} = this.state
+        if(id && password && !loading && mask){
             this.setState({ loading: true})
             Fetch('login/',
             {
@@ -125,6 +149,12 @@ export default class login extends Component{
                 icon: 'none',
                 title: '账号或密码不能为空'
             });
+        }
+        if(!mask){
+            Taro.showToast({
+                icon: 'none',
+                title: '请同意相关条例'
+            }); 
         }       
     }
 
@@ -140,7 +170,7 @@ export default class login extends Component{
     }
 
     render(){ 
-        const { id, password, showView, loading, powered } = this.state
+        const { id, password, showView, loading, powered ,mask ,li} = this.state
         return(
             <View className='container'>
                 {loading&&<View className='loading'></View>}
@@ -153,6 +183,9 @@ export default class login extends Component{
                {powered
                ?<Button className='login' onClick={this.onHandleLogin.bind(this)}>登录</Button>
                 :<Button open-type='getUserInfo' className='login' onClick={this.power}>登录</Button>}
+                <View className="content"><View className='cir' onClick={this.onHandleMask.bind(this)}>{mask?<View className='point'></View>:null}</View><View className='text' onClick={this.onHandleLi.bind(this)}>【孤独星球】用户相关条例和隐私政策</View></View>
+                {li?null:<Toast onHandleClose ={this.handleClose.bind(this)} />}
+                <View className='footer'><View className='text1'>反馈意见</View><View className='text2'>designed by muxi</View></View>
             </View>
         )
     }
